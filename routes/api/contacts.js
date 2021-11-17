eae137cf94cdded5d9bbf4370f3a2cbd9ba0f94c
Yana Vovk <71869556+Yana-Vovk@users.noreply.github.com>
2021-11-17 10:06:52 +0200

    Add routes

--- routes/api/contacts.js
+++ routes/api/contacts.js
@@ -1,24 +1,15 @@
-const express = require('express')
-const router = express.Router()
+const express = require("express");
+const router = express.Router();
+const ctrl = require("../../controllers/contactsCtrl");
+const validate = require("./validation");
 
-router.get('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router
+  .get("/", ctrl.listContacts)
+  .post("/", validate.addContact, ctrl.addContact);
 
-router.get('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
+router
+  .get("/:contactId", ctrl.getContactById)
+  .delete("/:contactId", ctrl.removeContact)
+  .put("/:contactId", validate.updateContact, ctrl.updateContact);
 
-router.post('/', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
-
-router.delete('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
-
-router.patch('/:contactId', async (req, res, next) => {
-  res.json({ message: 'template message' })
-})
-
-module.exports = router
+module.exports = router;
---
+++ routes/api/validation.js
@@ -0,0 +1,35 @@
+const Joi = require("joi");
+
+const schemaCreate = Joi.object({
+  name: Joi.string().alphanum().min(3).max(30).required(),
+
+  email: Joi.string().email({ minDomainSegments: 2 }),
+  phone: Joi.string().required(),
+});
+
+const schemaUpdate = Joi.object({
+  name: Joi.string().alphanum().min(3).max(30).required(),
+
+  email: Joi.string().email({ minDomainSegments: 2 }),
+  phone: Joi.string().optional(),
+}).min(1);
+
+const validate = (schema, obj, next) => {
+  const { error } = schema.validate(obj);
+  if (error) {
+    const [{ message }] = error.details;
+    return next({
+      status: 400,
+      message: `Failed: ${message.replace(/"/g, "")}`,
+    });
+  }
+  next();
+};
+
+module.exports.addContact = (req, _res, next) => {
+  return validate(schemaCreate, req.body, next);
+};
+
+module.exports.updateContact = (req, _res, next) => {
+  return validate(schemaUpdate, req.body, next);
+};
